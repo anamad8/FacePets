3169eb01ce6d5dde37d4f40cabba85be3428ba50
anamad8 <stellam_anabel@hotmail.com>
2023-01-18 15:17:56 -0300

    se quito comentarios

--- frontend/src/Components/DarkMode/DarkMode.jsx
+++ frontend/src/Components/DarkMode/DarkMode.jsx
@@ -4,27 +4,7 @@ import { DataContext } from '../../Contex/DataContex';
 
 function DarkMode() {
 
-    const { darkMode, setDarkMode, handlePosition } = useContext(DataContext)
-
-//     function handlePosition() {
-//         setDarkMode(!darkMode)
-//         let modo = document.getElementById("active");
-//         let body = document.body;
-
-//         let val=body.classList.toggle("active")
-//         localStorage.setItem("modo",val)
-        
-
-//         let valor=localStorage.getItem("modo")
-
-//         if (valor=="true") {
-//             body.classList.add("active")
-//         } else {
-//             body.classList.remove("active")
-//         }
-// }
-
-
+    const { darkMode, handlePosition } = useContext(DataContext)
 
     return (
 
@@ -35,12 +15,6 @@ function DarkMode() {
                 :
                 <button id="btn-darkMode" className='claro' onClick={handlePosition}><span class="toggle--label-background-claro"></span></button>
             }
-            {/* <button id="btn-darkMode" className='' onClick={handlePosition}></button> */}
-            {/* <input type="checkbox" id="toggle" className="toggle--checkbox" value={darkMode} onClick={handlePosition}/>
-            <label htmlFor="toggle" className="toggle--label">
-            <span className="toggle--label-background"></span>
-            </label>
-            <div className="background"></div> */}
         </div>
     )
 }
