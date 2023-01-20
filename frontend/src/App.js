53ca8bea8f0c25a981657756cd97ea5ca0f1a6e7
Marcelo Diego Albano <marce.d.a.2002@gmail.com>
2023-01-19 21:24:50 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
       user ? (
         <button onClick={logout}>Logout</button> 
       ) : (
-        <br/>
+        <></>
       )
       // ) : (
       //   <button onClick={login}>Login</button> 
