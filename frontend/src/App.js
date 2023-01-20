a3598bffc1ade30168a57badb4db3bcacb6a54c2
Marcelo Diego Albano <marce.d.a.2002@gmail.com>
2023-01-19 23:02:50 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,22 +59,4 @@ function App() {
   );
 }
 
-// function Navigation() {
-//   return (
-//     <nav>
-//       <ul>
-//         <li>
-//           <Link to="/home">Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/login">Login</Link>
-//         </li>
-//         <li>
-//           <Link to="/register">Register</Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// }
-
 export default App;
