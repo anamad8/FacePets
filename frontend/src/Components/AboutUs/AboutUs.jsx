57c9e15af95613a3a45d1ecab0be2f6762286301
Facu Bustos <Facubustos2107@gmail.com>
2023-01-23 03:47:26 -0300

    Cambios en AboutUs y en colores de interfaz
    Realice cambio en las cards de aboutUs. ( No esta terminado aun ) tambien realice cambios en los colores de interfaz para una navegacion mas moderna y simple.

--- frontend/src/Components/AboutUs/AboutUs.jsx
+++ frontend/src/Components/AboutUs/AboutUs.jsx
@@ -1,81 +1,71 @@
 import React from 'react'
-import ana from '../Img/ana.jpeg';
-import marce from '../Img/marce.jpeg';
-import nahue from '../Img/nahue.jpeg';
-import facu1 from '../Img/facu1.jpeg';
+import anabelamad from '../Img/anabelamad.png';
+import marceloalbano from '../Img/marceloalbano.png';
+import nahuelkubin from '../Img/nahuelkubin.png';
+import facub from '../Img/facub.png';
 import './AboutUsCards.css';
 
-
-
 function AboutUs() {
-
   return (
-    
-    <> 
-     
+    <> <div className='aboutus'><h1>¿Quienes somos?</h1>
+      <p>¡Aca te contamos!</p>
+    </div>
      <div className="contenedor">
       <div className="card">
         <div className="face front">
-          <img src={facu1} alt="" />
+          <img src={facub} alt="" />
           <h3>Facundo Bustos</h3>
         </div>
         <div className="face back">
           <h3>Facundo Bustos</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores repellendus necessitatibus recusandae in? Suscipit explicabo sequi accusantium nemo repudiandae, dicta voluptatum totam iste commodi ad quasi, incidunt rerum ex laudantium.</p>
+          <p>Hola, Yo soy Facundo! Pero la gente de confianza me dice memo. Vivo en mar del plata y tengo 26 años, me encanta la tecnologia, los videojuegos y claro tambien viajar. Estoy estudiando programación en CoderHouse, tambien en DevPlace y estoy inscripto en la UTN para este ciclo 2023. Si queres saber mas sobre mi te dejo mi LinkedIN</p>
           <div className="link">
-            <a href="https://www.linkedin.com/in/facundo-bustos-b86414251/" target='_blank'>Acceder al LinkedIN</a>
+            <a href="https://www.linkedin.com/in/facundo-bustos-b86414251/" target='_blank'>Acceder mi LinkedIN</a>
           </div>
         </div>
       </div>
       <div className="card">
         <div className="face front">
-          <img src={ana} alt="" />
+          <img src={anabelamad} alt="" />
           <h3>Anabel Amad</h3>
         </div>
         <div className="face back">
           <h3>Anabel Amad</h3>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores repellendus necessitatibus recusandae in? Suscipit explicabo sequi accusantium nemo repudiandae, dicta voluptatum totam iste commodi ad quasi, incidunt rerum ex laudantium.</p>
           <div className="link">
-            <a href="https://www.linkedin.com/in/anabel-amad-" target='_blank'>Acceder al LinkedIN</a>
+            <a href="https://www.linkedin.com/in/anabel-amad-" target='_blank'>Acceder mi LinkedIN</a>
           </div>
-
         </div>
       </div>
       <div className="card">
         <div className="face front">
-          <img src={marce} alt="" />
+          <img src={marceloalbano} alt="" />
           <h3>Marcelo Albano </h3>
         </div>
         <div className="face back">
-          <h3>Anabel Albano</h3>
+          <h3>Marcelo Albano</h3>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores repellendus necessitatibus recusandae in? Suscipit explicabo sequi accusantium nemo repudiandae, dicta voluptatum totam iste commodi ad quasi, incidunt rerum ex laudantium.</p>
           <div className="link">
-            <a href="https://www.linkedin.com/in/marcelo-diego-albano-6a453022b" target='_blank'>Acceder al LinkedIN</a>
+            <a href="https://www.linkedin.com/in/marcelo-diego-albano-6a453022b" target='_blank'>Acceder mi LinkedIN</a>
           </div>
 
         </div>
       </div>
       <div className="card">
         <div className="face front">
-          <img src={nahue} alt="" />
+          <img src={nahuelkubin} alt="" />
           <h3>Nahuel Kubin</h3>
         </div>
         <div className="face back">
           <h3>Nahuel Kubin</h3>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores repellendus necessitatibus recusandae in? Suscipit explicabo sequi accusantium nemo repudiandae, dicta voluptatum totam iste commodi ad quasi, incidunt rerum ex laudantium.</p>
           <div className="link">
-            <a href="https://www.linkedin.com/in/rodolfo-nahuel-kubin-346b69253/"target='_blank'>Acceder al LinkedIN</a>
+            <a href="https://www.linkedin.com/in/rodolfo-nahuel-kubin-346b69253/"target='_blank'>Acceder mi LinkedIN</a>
           </div>
-
         </div>
       </div>
-
-
     </div>
 </>
-
-
-
   )
 }
 
